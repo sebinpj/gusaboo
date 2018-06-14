0add08214063dee4b966704c15bd3c416007f776
Sebin P Johnson <sebin.johnson@entapza.com>
2018-06-14 09:18:25 +0530

    Minor reload fix

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var ejs = require('gulp-ejs')
 var log = require('fancy-log')
 var inject = require('gulp-inject');
 var htmlbeautify = require('gulp-html-beautify');
-
+var cache = require('gulp-cache')
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function () {
@@ -13,52 +13,62 @@ gulp.task('serve', ['sass'], function () {
         server: "./app"
     });
     //checking for any new asset change
-    gulp.watch("assets/scss/*.scss", ['compile']).on('change',browserSync.reload);
-    gulp.watch("assets/css/*.css",['compile']).on('change',browserSync.reload);
-    gulp.watch("assets/js/*.js",['compile']).on('change',browserSync.reload);
-    gulp.watch("templates/*.ejs", ['compile']).on('change',browserSync.reload);
-    gulp.watch("templates/**/*.ejs", ['compile']).on('change',browserSync.reload);
-    gulp.watch("templates/**/**/*.ejs", ['compile']).on('change',browserSync.reload);
-    gulp.watch("app/html/*.html").on('change', browserSync.reload);
+    gulp.watch("assets/scss/*.scss", ['compile']).on('change', browserSync.reload);
+    gulp.watch("assets/css/*.css", ['compile']).on('change', browserSync.reload);
+    gulp.watch("assets/js/*.js", ['compile']).on('change', browserSync.reload);
+    gulp.watch("assets/*.html", ['compile']);
+    gulp.watch("templates/**/*.ejs", ['compile']);
+    gulp.watch("templates/**/**/*.ejs", ['compile']);
+    gulp.watch("templates/*.ejs", ['compile']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS 
 gulp.task('sass', function () {
     return gulp.src("assets/scss/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("app/css"))
+        .pipe(gulp.dest("app/css", {
+            overwrite: true
+        }))
         .pipe(browserSync.stream());
 });
 
-//this compiles ejs to html
+//gulp-ejs seems buggy removing ejs 
+// //this compiles ejs to html
 gulp.task('ejs-compile', function () {
     gulp.src('./templates/*.ejs')
         .pipe(ejs({
             msg: 'Hello Gulp!'
         }, {}, {
             ext: '.html'
-            }).on('error', function (err) {
-                gulp.watch("templates/*.ejs", ['compile']);
-                gulp.watch("templates/**/*.ejs", ['compile']);                    
-        }))
-        .pipe(gulp.dest('./app'));
+        }).on('error', log))
+        .pipe(gulp.dest('./app', {
+            overwrite: true
+        }));
 });
 
 //this copies all resources to app folder
 gulp.task('copy', function () {
-    gulp.src('assets/css/*.css').pipe(gulp.dest('app/css'));
-    return gulp.src('assets/js/*.js').pipe(gulp.dest('app/js'));    
+    gulp.src('assets/css/*.css').pipe(gulp.dest('app/css', {
+        overwrite: true
+    }));
+    return gulp.src('assets/js/*.js').pipe(gulp.dest('app/js', {
+        overwrite: true
+    }))
+    //  gulp.src('assets/*.html')
+    //     .pipe(htmlbeautify())
+    //     .pipe(gulp.dest('app', {
+    //         overwrite: true
+    //     }));
 })
 
-//this beatifies html code
-gulp.task('htmlbeautify', function() {
-    gulp.src('./app/*.html')
-      .pipe(htmlbeautify())
-      .pipe(gulp.dest('./app/'))
- });
 
+
+gulp.task('clearCache', function () {
+    cache.clearAll();
+});
 //this tasks compile all necesary files and creates a dump in app/ folder
-gulp.task('compile', ['ejs-compile', 'htmlbeautify', 'sass', 'copy']);
+gulp.task('compile', ['clearCache','ejs-compile', 'sass', 'copy']);
 
 //this happens when you run gulp
 gulp.task('default', ['compile', 'serve']);
